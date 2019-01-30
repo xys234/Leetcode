4ecb7563aa453ea8c520e30251938b7e09a4f82d
yinw <xys234@gmail.com>
2019-01-30 18:57:37 -0500

    Add tree problems

--- Algo/search_in_rotated_array_II.py
+++ Algo/search_in_rotated_array_II.py
@@ -28,5 +28,101 @@ class Solution(object):
         """
         :type nums: List[int]
         :type target: int
-        :rtype: bool
+        :rtype: int
         """
+
+        res = False
+        if not nums:
+            return False
+
+        pivot = self.find_pivot(nums, 0, len(nums)-1)
+        if pivot == -1 or pivot == len(nums)-1:
+            res = self.binary_search(nums, target, 0, len(nums) - 1)
+        elif nums[pivot] == target:
+            res = pivot
+        else:
+            if nums[0] <= target:
+                res = self.binary_search(nums, target, 0, pivot)
+            else:
+                res = self.binary_search(nums, target, pivot+1, len(nums)-1)
+        return res >= 0
+
+    def binary_search(self, nums, target, low, high):
+        """
+        Standard binary search in an array with distinct elements
+        :param nums:
+        :param target:
+        :param low:
+        :param high:
+        :return:
+        """
+
+        while low <= high:
+            mid = int((low + high) / 2)  # floor
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
+
+    def find_pivot(self, nums, low, high):
+        """
+        find the index of the pivot element in the array;
+        :param nums:
+        :param low:
+        :param high:
+        :return:
+
+        for [3, 4, 5, 6, 1, 2]. return 3 (the index of 6)
+        if it is fully sorted,  return the last element
+
+        """
+
+        if low > high:
+            return -1
+
+        if low == high:
+            return low
+
+        mid = int((low+high)/2)
+        if mid < high and nums[mid] > nums[mid+1]:
+            return mid
+        if mid > low and nums[mid] < nums[mid-1]:
+            return mid-1
+        if nums[low] >= nums[mid]:
+            return self.find_pivot(nums, low, mid-1)
+        return self.find_pivot(nums, mid+1, high)
+
+
+if __name__=='__main__':
+
+    sol = Solution()
+
+    cases = [
+        # (sol.find_pivot, ([3,4,5,6,7,1,2], 0, 6), 4),
+        # (sol.find_pivot, ([1,2,3,4], 0, 3), -1),
+        (sol.find_pivot, ([1,1,3], 0, 2), 2),
+        # (sol.binary_search, ([1,2,3,4], 2, 0, 3), 1),
+        # (sol.binary_search, ([1,2,3,4], 5, 0, 3), -1),
+        # (sol.binary_search, ([1,2,3,4], 0, 0, 3), -1),
+        # (sol.binary_search, ([1], 0, 0, 0), -1),
+        # (sol.binary_search, ([1], 1, 0, 0), 0),
+        # (sol.binary_search, ([1, 1, 3], 3, 0, 2), 2),
+        # (sol.search, ([4,5,6,7,0,1,2], 0), True),
+        # (sol.search, ([4,5,6,7,0,1,2], 3), False),
+        # (sol.search, ([], 3), False),
+        # (sol.search, ([1], 1), True),
+        # (sol.search, ([1,1,3], 3), True),
+        # (sol.search, ([3,1], 3), True),
+        # (sol.search, ([2,5,6,0,0,1,2], 0), True),
+        # (sol.search, ([2,5,6,0,0,1,2], 3), False),
+             ]
+
+    for i, (func, case, expected) in enumerate(cases):
+        ans = func(*case)
+        if ans == expected:
+            print("Case {:d} Passed".format(i + 1))
+        else:
+            print("Case {:d} Failed; Expected {:s} != {:s}".format(i+1, str(expected), str(ans)))
