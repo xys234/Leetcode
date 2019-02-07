9672f05978cf445843f17b7d8fdae939237abe98
xys234@gmail.com <wyin3@gatech.edu>
2019-02-06 23:42:28 -0500

    review

--- Algo/best_time_to_buy_and_sell_stock_III.py
+++ Algo/best_time_to_buy_and_sell_stock_III.py
@@ -9,9 +9,14 @@ Design an algorithm to find the maximum profit. You may complete at most two tra
 Note:
 You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
 
-"""
 
-"""
+dp[transaction_id][day_id] = max(dp[transaction_id-1][day_id],
+      dp[transaction_id-1][day_0~day_id] + prices[day_id]- prices[day_0~day_id])
+
+On ith day, either no trasaction, or one more transaction based on all possible previous transactions
+
+bETTER SOLUTION
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/discuss/213474/DP-Java-Solution-for-k-transactions
 
 Algorithmic notes
 
@@ -52,29 +57,6 @@ TRADE		(1,1)	(1,2)	(1,3)	(1,3)	(1,5)	(1,6)	(1,6)	(1,6)	(1,9)	(1,9)
 	9										8-9
 MAX	10	"-INF"	-1	-1	-1	1	1	1	4	4	4
 TRADE		(1,1)	(1,2)	(1,3)	(1,3)	(4,5)	(4,6)	(4,6)	(7,8)	(7,9)	(7,9)
-												
-												
-												
-												
-												
-												
-												
-												
-												
-												
-												
-												
-												
-												
-												
-												
-												
-												
-
-
-
-
-
 
 
 """
@@ -102,6 +84,23 @@ class Solution:
             min_price = min(p, min_price)
             max_profit = max(p - min_price, max_profit)
         return max_profit
+
+    def maxProfit(self, prices: 'List[int]') -> 'int':
+        """
+            TLE
+
+        """
+        if len(prices) <= 1:
+            return 0
+
+        MAX_NUM_TRANSACTIONS = 2
+        n = len(prices)
+        dp = [[0 for _ in range(n + 1)] for _ in range(1 + MAX_NUM_TRANSACTIONS)]
+        for i in range(1, n + 1):
+            for t in range(1, MAX_NUM_TRANSACTIONS + 1):
+                for j in range(1, i + 1):
+                    dp[t][i] = max(dp[t][i], dp[t][i-1], dp[t - 1][i], dp[t - 1][j] + prices[i-1] - prices[j-1])
+        return dp[MAX_NUM_TRANSACTIONS][n]
 
 
     def maxProfit_Ktrade(self, k, prices):
@@ -150,13 +149,21 @@ class Solution:
 
 
 
-if __name__=="__main__":
-    sol = Solution()
-    # p = [7, 1, 5, 3, 6, 5]
-    # p = [1,2,4]
-    # p = [2,1,2,0,1]
-    p = [1,2,4,2,5,7,2,4,9,0]
-    profit, trades = sol.maxProfit_Ktrade(2, p)
 
-    print(profit[1:])
-    print(trades)
+if __name__=='__main__':
+
+    sol = Solution()
+
+    cases = [
+
+        (sol.maxProfit, ([3,3,5,0,0,3,1,4],), 6),
+        (sol.maxProfit, ([6,1,3,2,4,7],), 7),
+
+             ]
+
+    for i, (func, case, expected) in enumerate(cases):
+        ans = func(*case)
+        if ans == expected:
+            print("Case {:d} Passed".format(i + 1))
+        else:
+            print("Case {:d} Failed; Expected {:s} != {:s}".format(i+1, str(expected), str(ans)))
