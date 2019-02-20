d440e66f41bef2fc87cf1315887a96df0ed23107
yinw <xys234@gmail.com>
2019-02-20 17:32:42 -0500

    17

---
+++ Algo/letter_combination_of_a_phone_number.py
@@ -0,0 +1,20 @@
+"""
+
+17.
+
+Given a string containing digits from 2-9 inclusive,
+return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+Note that 1 does not map to any letters.
+
+
+"""
+
+
+class Solution:
+    def letterCombinations(self, digits: 'str') -> 'List[str]':
+        digit_to_letters = {
+            2:
+
+        }
