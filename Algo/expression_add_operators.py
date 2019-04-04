242a1caf17abf4b08d4ce9388dd643e1b662519d
yinw <xys234@gmail.com>
2019-04-04 17:51:22 -0400

    expression_add_operators

--- Algo/expression_add_operators.py
+++ Algo/expression_add_operators.py
@@ -31,15 +31,57 @@ Output: []
 
 
 class Solution:
-    def addOperators(self, num: str, target: int):
-        """
+    def addOperators(self, num: 'str', target: 'int') -> 'List[str]':
 
-        :param num:
-        :param target:
-        :rtype list
-        """
+        N = len(num)
+        answers = []
+        def recurse(index, prev_operand, current_operand, value, string):
 
-        pass
+            # Done processing all the digits in num
+            if index == N:
+
+                # If the final value == target expected AND
+                # no operand is left unprocessed
+                if value == target and current_operand == 0:
+                    answers.append("".join(string[1:]))
+                return
+
+            # Extending the current operand by one digit
+            current_operand = current_operand*10 + int(num[index])
+            str_op = str(current_operand)
+
+            # To avoid cases where we have 1 + 05 or 1 * 05 since 05 won't be a
+            # valid operand. Hence this check
+            if current_operand > 0:
+
+                # NO OP recursion
+                recurse(index + 1, prev_operand, current_operand, value, string)
+
+            # ADDITION
+            string.append('+')
+            string.append(str_op)
+            recurse(index + 1, current_operand, 0, value + current_operand, string)
+            string.pop()
+            string.pop()
+
+            # Can subtract or multiply only if there are some previous operands
+            if string:
+
+                # SUBTRACTION
+                string.append('-')
+                string.append(str_op)
+                recurse(index + 1, -current_operand, 0, value - current_operand, string)
+                string.pop()
+                string.pop()
+
+                # MULTIPLICATION
+                string.append('*')
+                string.append(str_op)
+                recurse(index + 1, current_operand * prev_operand, 0, value - prev_operand + (current_operand * prev_operand), string)
+                string.pop()
+                string.pop()
+        recurse(0, 0, 0, 0, [])
+        return answers
 
 
 if __name__ == '__main__':
@@ -48,8 +90,8 @@ if __name__ == '__main__':
 
     cases = [
 
-        (sol.addOperators, ("aab", ), [["aa","b"], ["a","a","b"]]),
-        (sol.addOperators, ("bb", ), [["b","b"], ["bb"]]),
+        # (sol.addOperators, ("115", 115), ["115"]),
+        (sol.addOperators, ("1051", 1), ["10+5"]),
 
              ]
 
