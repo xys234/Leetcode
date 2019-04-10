281e2a8cfbd6e509b476f96adb6ba6cfa4d8b3d3
yinw <xys234@gmail.com>
2019-04-10 18:03:57 -0400

    306

--- Algo/search/split_array_into_fib_sequence.py
+++ Algo/search/split_array_into_fib_sequence.py
@@ -59,6 +59,31 @@ class Solution:
             if self.dfs(num_str[i + 1:], path + [int(curr)], res):
                 return True
 
+    def split_fib(self, s):
+
+        MAX = 2 ** 31
+        n = len(s)
+        res = []
+
+        def recurse(current_seq, start_index):
+            if start_index >= n and len(current_seq) >= 3 and current_seq[-1] == current_seq[-2] + current_seq[-3]:
+                res.extend(current_seq)
+                return True
+
+            if len(current_seq) >= 3 and current_seq[-1] != current_seq[-2] + current_seq[-3]:
+                return False
+
+            for i in range(start_index, n):
+                num_str = s[start_index:i+1]
+                num = int(num_str)
+                if len(num_str) > 1 and num_str[0] == '0' or num >= MAX:
+                    continue
+                if recurse(current_seq + [num], i + 1):
+                    return True
+
+        recurse([], 0)
+        return res
+
 
 if __name__ == '__main__':
 
@@ -66,8 +91,9 @@ if __name__ == '__main__':
 
     cases = [
 
-        (sol.splitIntoFibonacci, ("123456579", ), [123, 456, 579]),
-        # (sol.splitIntoFibonacci, ("1051", 1), ["10+5"]),
+        (sol.split_fib, ("123456579", ), [123, 456, 579]),
+        (sol.split_fib, ("11235813", ), [1,1,2,3,5,8,13]),
+        (sol.splitIntoFibonacci, ("112358130", ), []),
 
              ]
 
