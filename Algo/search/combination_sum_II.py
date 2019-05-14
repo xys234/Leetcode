fab0decc5d887acc7bb6bbab562addc228106998
xys234@gmail.com <wyin3@gatech.edu>
2019-05-13 22:28:14 -0400

    133

--- Algo/search/combination_sum_II.py
+++ Algo/search/combination_sum_II.py
@@ -34,5 +34,50 @@ A solution set is:
 """
 
 class Solution:
-    def combinationSum2(self, candidates: 'List[int]', target: 'int') -> 'List[List[int]]':
-        pass
+    def combinationSum2(self, candidates, target):
+
+        res = set()
+        candidates.sort()
+        n = len(candidates)
+
+        def recurse(solution, start_index, t):
+            if t == 0:
+                res.add(solution)
+                return
+
+            if start_index >= n:
+                return
+
+            for i in range(start_index, n):
+                new_target = t - candidates[i]
+                if new_target < 0:
+                    break
+                recurse(solution+(candidates[i],), i+1, new_target)
+
+        recurse(tuple(), 0, target)
+        return [list(s) for s in res]
+
+
+if __name__=='__main__':
+
+    def compare_list(ans, expected, type='value'):
+        if type == 'equality':
+            return ans == expected
+        if type == 'item':
+            return sorted(ans) == sorted(expected)
+
+    sol = Solution()
+
+    cases = [
+
+        (sol.combinationSum2, ([2,3,6,7], 7), [[7]]),
+        (sol.combinationSum2, ([2,3,5], 8), [[3,5]]),
+
+             ]
+
+    for i, (func, case, expected) in enumerate(cases):
+        ans = func(*case)
+        if compare_list(ans, expected, 'item'):
+            print("Case {:d} Passed".format(i + 1))
+        else:
+            print("Case {:d} Failed; Expected {:s} != Output {:s}".format(i+1, str(expected), str(ans)))
