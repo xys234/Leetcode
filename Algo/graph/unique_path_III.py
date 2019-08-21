b55ee217b22fe2e8b56ade8db6947e7ecfaeac62
xys234@gmail.com <wyin3@gatech.edu>
2019-08-20 23:27:49 -0400

    lc 996

--- Algo/graph/unique_path_III.py
+++ Algo/graph/unique_path_III.py
@@ -71,6 +71,38 @@ class Solution:
         dfs(origin[0], origin[1], unvisited, ans)
         return ans[0]
 
+    def uniquePathsIII_solution(self, grid):
+        R, C = len(grid), len(grid[0])
+
+        def neighbors(r, c):
+            for nr, nc in ((r-1, c), (r, c-1), (r+1, c), (r, c+1)):
+                if 0 <= nr < R and 0 <= nc < C and grid[nr][nc] in {0, 2}:
+                    yield nr, nc
+
+        todo = 0
+        for r, row in enumerate(grid):
+            for c, val in enumerate(row):
+                if val != -1: todo += 1
+                if val == 1: sr, sc = r, c
+                if val == 2: tr, tc = r, c
+
+        self.ans = 0
+
+        def dfs(r, c, todo):
+            todo -= 1
+            if todo < 0: return
+            if r == tr and c == tc:
+                if todo == 0:
+                    self.ans += 1
+                return
+
+            grid[r][c] = -1
+            for nr, nc in neighbors(r, c):
+                dfs(nr, nc, todo)
+            grid[r][c] = 0
+
+        dfs(sr, sc, todo)
+        return self.ans
 
 if __name__ == '__main__':
 
