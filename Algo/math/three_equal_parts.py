22ca8b2d262114920cdcc1f959b49b65b40b084f
yinw <xys234@gmail.com>
2019-08-28 19:11:25 -0400

    lc

--- Algo/math/three_equal_parts.py
+++ Algo/math/three_equal_parts.py
@@ -12,66 +12,57 @@ class Solution:
         if n % 3 != 0:
             return [-1, -1]
 
-        target = n / 3
+        target_ones = int(n / 3)
+        if target_ones == 0:
+            return [0, m-1]
+
         target_trailing_zeros = 0
-        first, third = -1, -1
-        current_ones = 0
-        for j in range(m-1, -1, -1):
-            if A[j] == 1:
-                current_ones += 1
-                if current_ones == target:
-                    third = j
-                    break
-            else:
+        for j in range(m - 1, -1, -1):
+            if A[j] == 0:
                 target_trailing_zeros += 1
-
-        current_ones = 0
-        current_trailing_zeros = 0
-        for i in range(third-1):
-            if A[i] == 1:
-                current_ones += 1
-                if current_ones == target:
-                    first = i
-                    break
-
-        # fail to find the first partition
-        if first < 0:
-            return [-1, -1]
-        i = first+1
-        while current_trailing_zeros != target_trailing_zeros:
-            if A[i] == 0:
-                current_trailing_zeros += 1
-                first += 1
             else:
                 break
 
-        # not enough trailing zeros
-        if current_trailing_zeros != target_trailing_zeros:
+        def find_group(start, target_length=None):
+            end = -1
+            current_ones, current_trailing_zeros, first_one = 0, 0, -1
+            for i in range(start, m):
+                if A[i] == 1:
+                    current_ones += 1
+                    if current_ones == 1:
+                        first_one = i
+                else:
+                    if current_ones == target_ones:
+                        current_trailing_zeros += 1
+                    else:
+                        if target_length and current_ones >= 1 and i-first_one+1 > target_length:
+                            return -1
+
+                if current_ones == target_ones and current_trailing_zeros == target_trailing_zeros:
+                    end = i
+                    break
+            return first_one, end
+
+        def next_one(start):
+            ind = -1
+            for i in range(start, m):
+                if A[i] == 1:
+                    ind = i
+                    break
+
+            return ind
+
+        first_one, first_end = find_group(0)
+        second_one = next_one(first_end+1)
+        second_end = second_one + (first_end - first_one + 1) - 1
+        if A[first_one:first_end+1] != A[second_one:second_end+1]:
             return [-1, -1]
 
-        # find the middle partition
-        current_ones = 0
-        second = first + 1
-        while second < third:
-            if A[second] == 1:
-                current_ones += 1
-                if current_ones == target:
-                    break
-            second += 1
-
-        if current_ones != target:
+        third_one = next_one(second_end+1)
+        if A[first_one:first_end+1] != A[third_one:]:
             return [-1, -1]
 
-        current_trailing_zeros = 0
-        while second < third:
-            if A[second] == 0:
-                current_trailing_zeros += 1
-                if current_trailing_zeros == target_trailing_zeros:
-                    break
-            second += 1
-
-        if current_trailing_zeros == target_trailing_zeros:
-            return [first, second+1]
+        return [first_end, second_end+1]
 
 
 if __name__ == '__main__':
@@ -80,8 +71,11 @@ if __name__ == '__main__':
 
     cases = [
         (method, ([1,0,1,0,1],), [0, 3]),
-        # (method, ([1,1,0,1,1],), [-1, -1]),
-        # (method, ([1,0,1,1,0],), [-1, -1]),
+        (method, ([1,1,0,1,1],), [-1, -1]),
+        (method, ([1,0,1,1,0],), [-1, -1]),
+        (method, ([0,0,0,0,0],), [0, 4]),
+        (method, ([0,1,0,1,1,0,0,1,0,1,0,0,0,0,1,0,1,1,1,0],), [-1, -1]),
+        (method, ([1,0,1,0,1,0,1,1,1],), [-1, -1]),
     ]
 
     for i, (func, case, expected) in enumerate(cases):
