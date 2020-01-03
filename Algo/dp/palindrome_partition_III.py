a2ee7a5de1a8afdfbecdb2f77a04ea8120967561
yinw <xys234@gmail.com>
2020-01-03 17:34:57 -0500

    lc 1278

--- Algo/dp/palindrome_partition_III.py
+++ Algo/dp/palindrome_partition_III.py
@@ -33,7 +33,33 @@ Output: 0
 
 class Solution:
     def palindromePartition(self, s: str, k: int) -> int:
-        pass
+
+        def palindrome_cost(s):
+            cost = [[0 for _ in s] for _ in s]
+            n = len(s)
+            for i in range(n-1):
+                cost[i][i+1] = int(s[i] != s[i+1])
+
+            for l in range(3, n+1):
+                for i in range(n-l+1):
+                    cost[i][i+l-1] = cost[i+1][i+l-2] + int(s[i] != s[i+l-1])
+            return cost
+
+        cost = palindrome_cost(s)
+        n = len(s)
+
+        if n == k:
+            return 0
+
+        dp = [[float('inf') for _ in range(k + 1)] for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            dp[i][1] = cost[0][i-1]     # first i elements into 1 group
+
+        for k in range(2, k + 1):
+            for i in range(k, n + 1):
+                for j in range(1, i):
+                    dp[i][k] = min(dp[i][k], dp[j][k - 1] + cost[j][i-1])
+        return dp[-1][-1]
 
 
 if __name__=='__main__':
